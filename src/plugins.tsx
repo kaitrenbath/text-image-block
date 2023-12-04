357aa301f127710e548717c67e173767450dd194
kai <kai.trenbath@gmail.com>
2023-12-04 16:19:49 +0000

    feat: add text styles

--- src/plugins.tsx
+++ src/plugins.tsx
@@ -1,13 +1,20 @@
 import {
+    AlignCenterPlugin,
+    AlignJustifyPlugin,
+    AlignLeftPlugin,
+    AlignRightPlugin,
     BoldPlugin,
     ItalicPlugin,
     LinkPlugin,
+    OrderedListPlugin,
     PluginComposer,
+    ResetFormattingPlugin,
     StrikethroughPlugin,
     TextStylePlugin,
     UnderlinePlugin,
+    UnorderedListPlugin,
 } from '@frontify/fondue';
-import { TextStylePluginsWithoutImage } from '@frontify/guideline-blocks-settings';
+import { TextStylePluginsWithoutImage, TextStylesWithoutImage } from '@frontify/guideline-blocks-settings';
 
 export const getPlugins = () => {
     const plugins = new PluginComposer();
@@ -23,6 +30,23 @@ export const getPlugins = () => {
         new UnderlinePlugin(),
         new StrikethroughPlugin(),
         new LinkPlugin(),
+    ]);
+    plugins.setPlugin([
+        new AlignLeftPlugin({
+            validTypes: TextStylesWithoutImage,
+        }),
+        new AlignCenterPlugin({
+            validTypes: TextStylesWithoutImage,
+        }),
+        new AlignRightPlugin({
+            validTypes: TextStylesWithoutImage,
+        }),
+        new AlignJustifyPlugin({
+            validTypes: TextStylesWithoutImage,
+        }),
+        new UnorderedListPlugin(),
+        new OrderedListPlugin(),
+        new ResetFormattingPlugin(),
     ]);
 
     return plugins;
